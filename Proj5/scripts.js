7c45020b2f014878da96306a831f9fab079cb886
ScofieldaCreep <tju_scofield@outlook.com>
2024-10-27 00:00:22 -0400

    proj 5

--- Proj5/scripts.js
+++ Proj5/scripts.js
@@ -1,6 +1,7 @@
 const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
 let load = 0
+// 这样操控 html 中的数值变量
 let int = setInterval(blurring, 30)
 function blurring() {
 	load++
@@ -8,6 +9,7 @@ function blurring() {
 		clearInterval(int)
 	}
 	loadText.innerText = `${load}%`
+	// 可以这样操控 元素的 css 样式
 	loadText.style.opacity = scale(load, 0, 100, 1, 0)
 	bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
