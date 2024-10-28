613adaf30d2e8d03bde16b2d150668e63c44a50b
ScofieldaCreep <tju_scofield@outlook.com>
2024-10-28 00:41:27 -0400

    proj 8

--- Proj8/scripts.js
+++ Proj8/scripts.js
@@ -1,15 +1,11 @@
-const left = document.querySelector('.left')
-const right = document.querySelector('.right')
-const container = document.querySelector('.container')
+const labels = document.querySelectorAll('.form-control label')
 
-left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
-right.addEventListener('mouseenter', () =>
-	container.classList.add('hover-right')
-)
-
-left.addEventListener('mouseleave', () =>
-	container.classList.remove('hover-left')
-)
-right.addEventListener('mouseleave', () =>
-	container.classList.remove('hover-right')
-)
+labels.forEach(label => {
+	label.innerHTML = label.innerText
+		.split('')
+		.map(
+			(letter, idx) =>
+				`<span style="transition-delay: ${idx * 20}ms">${letter}</span>`
+		)
+		.join('')
+})
